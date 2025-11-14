0cc87bc1c59ba4def407121d00fa3f48ab20863a
slwmoose <>
2025-11-14 22:24:41 +0000

    Slicing & Concatenation

---
+++ variables_and_types/slicing_concatenation/main.py
@@ -0,0 +1,15 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+
+dairy1 = grocery_items[0:4]
+
+dairy2 = grocery_items[5:11]
+
+bakery1 = grocery_items[12:17]
+
+announcement = "We have dairy and bakery items:"
+
+list = dairy1 + ", " + dairy2 + ", and " + bakery1
+
+location = "in aisle 5"
+
+print (announcement +" " + list + " " + location)
